51c5355e598cd395db9ffc26376d0e75cb7133ae
Lane876 <milenkovicmilan10@gmail.com>
2020-03-18 00:35:14 +0100

    Updates

---
+++ precache-manifest.9f77e64b1572240a02b8971ea78d2c4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91385bb10c1d0151f58e8e339b30193",
+    "url": "/portfolioReact/index.html"
+  },
+  {
+    "revision": "1489350c9acae2eac02a",
+    "url": "/portfolioReact/static/css/main.73b32d4b.chunk.css"
+  },
+  {
+    "revision": "6d63275dfbce1981110c",
+    "url": "/portfolioReact/static/js/2.74290d08.chunk.js"
+  },
+  {
+    "revision": "989ac8b04042782519a29910086adb47",
+    "url": "/portfolioReact/static/js/2.74290d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1489350c9acae2eac02a",
+    "url": "/portfolioReact/static/js/main.904e8d7e.chunk.js"
+  },
+  {
+    "revision": "e245cc584277efa59140",
+    "url": "/portfolioReact/static/js/runtime-main.65b0f2ad.js"
+  },
+  {
+    "revision": "dff53aa28b9502cccccfeca27eb62523",
+    "url": "/portfolioReact/static/media/hotel.dff53aa2.png"
+  }
+]);
